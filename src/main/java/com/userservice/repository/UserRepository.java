30a53b750fcec4a8b83375e1584f088d6268fafd
Likhitha <122144474+Likhi-G1@users.noreply.github.com>
2023-09-28 11:34:20 +0530

    Delete src directory

--- src/main/java/com/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.userservice.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // You can add custom query methods if needed
-}
